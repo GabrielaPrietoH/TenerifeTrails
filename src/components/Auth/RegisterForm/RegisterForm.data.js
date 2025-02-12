ac2adac889e931bbba2b5e3e52c0d26914c04376
GabrielaPrietoH <123327970+GabrielaPrietoH@users.noreply.github.com>
2025-02-12 10:31:25 +0000

    add form data

--- src/components/Auth/RegisterForm/RegisterForm.data.js
+++ src/components/Auth/RegisterForm/RegisterForm.data.js
@@ -10,8 +10,20 @@ export function initialValues() {
 
 export function validationSchema() {
     return Yup.object({
-        email: Yup.string().email("El email no es correcto").required("El email es obligatorio"),
-        password: Yup.string().required("La contraseña es obligatoria"),
-        repeatPassword: Yup.string().required("La contraseña es obligatoria").oneOf([Yup.ref('password')], "Las contraseñas no son iguales"),
+        email: Yup.string()
+            .email("El email no es correcto")
+            .required("El email es obligatorio"),
+        
+        password: Yup.string()
+            .min(8, "La contraseña debe tener al menos 8 caracteres")
+            .matches(/[A-Z]/, "La contraseña debe contener al menos una letra mayúscula")
+            .matches(/[a-z]/, "La contraseña debe contener al menos una letra minúscula")
+            .matches(/[0-9]/, "La contraseña debe contener al menos un número")
+            .matches(/[@$!%*?&]/, "La contraseña debe contener al menos un carácter especial (@, $, !, %, etc.)")
+            .required("La contraseña es obligatoria"),
+
+        repeatPassword: Yup.string()
+            .required("Repetir la contraseña es obligatorio")
+            .oneOf([Yup.ref('password')], "Las contraseñas no son iguales"),
     });
 }
