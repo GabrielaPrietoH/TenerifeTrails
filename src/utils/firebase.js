c012e0e0efd042f12f954fd4df675e200f70e3c1
GabrielaPrietoH <123327970+GabrielaPrietoH@users.noreply.github.com>
2025-03-18 18:00:34 +0000

    add reviewsScreen

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,13 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { getStorage } from 'firebase/storage'; 
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "Tu-API-Key",
+  apiKey: "AIzaSyDCa5ywEdMZlg10sgNAiz5CHooI5GUKovA",
   authDomain: "tenerife-trails.firebaseapp.com",
   projectId: "tenerife-trails",
   storageBucket: "tenerife-trails.firebasestorage.app",
@@ -18,4 +18,15 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(initializeApp);
+
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
+
+export { auth };
+
+// Initialize Firestore
+export const db = getFirestore(app);
+
+// Initialize Firebase Storage
+export const storage = getStorage(app); 
